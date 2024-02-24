2867ee9ad51778e9e8ff7062731f6f17ebf1f3a9
Francesco Palozzi <palozzifrancesco@libero.it>
2024-02-24 20:25:08 +0100

    11° parte

--- game/src/object/OBJ_Chest.java
+++ game/src/object/OBJ_Chest.java
@@ -5,13 +5,21 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+import main.GamePanel;
+
 public class OBJ_Chest extends SuperObject{
     
-    public OBJ_Chest() {
+    GamePanel gp;
+
+    public OBJ_Chest(GamePanel gp) {
+
+        this.gp = gp;
 
         name = "Chest";
         try {
             image = ImageIO.read(new FileInputStream("game/res/objects/chest.png"));
+            uTool.scaleImage(image, gp.tileSize, gp.tileSize);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
--- game/src/object/OBJ_Key.java
+++ game/src/object/OBJ_Key.java
@@ -5,13 +5,21 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+import main.GamePanel;
+
 public class OBJ_Key extends SuperObject{
     
-    public OBJ_Key() {
+    GamePanel gp;
 
+    public OBJ_Key(GamePanel gp) {
+
+        this.gp = gp;
+        
         name = "Key";
         try {
             image = ImageIO.read(new FileInputStream("game/res/objects/key.png"));
+            uTool.scaleImage(image, gp.tileSize, gp.tileSize);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
